175b74057d0cc2f41e33f93339fde22425d92187
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2024-10-10 13:29:34 -0600

    conv plantillas

--- PyLibrary/CW_Conversations.py
+++ PyLibrary/CW_Conversations.py
@@ -10,6 +10,137 @@ load_dotenv()
 cw_token = os.getenv('CW_TOKEN')
 base_url = os.getenv('BASE_URL')
 
+# Enumeración en Python para los buzones
+class ChatwootSenders:
+    Medicos = 2  
+    Pacientes = 4  
+
+def envia_mensaje_plantilla(contacto_id, plantilla, parametros=None, buzon=ChatwootSenders.Pacientes, bot_name=None):
+    """
+    Envía un mensaje usando una plantilla en Chatwoot.
+    
+    :param contacto_id: ID del contacto al que se le enviará el mensaje.
+    :param plantilla: Plantilla de mensaje con marcadores de posición ({{1}}, {{2}}, ...).
+    :param parametros: Lista de parámetros para reemplazar en la plantilla.
+    :param buzon: El buzón desde el que se enviará el mensaje (Pacientes o Medicos).
+    :param bot_name: Nombre del bot si es un mensaje automatizado.
+    """
+    if parametros is None:
+        parametros = []
+
+    print(f"Cantidad de parámetros: {len(parametros)} Texto: {plantilla} al contacto: {contacto_id} desde: {buzon}")
+
+    # Reemplazar los parámetros en la plantilla
+    text_to_send = plantilla
+    for ii, param in enumerate(parametros, 1):
+        text_to_send = text_to_send.replace(f"{{{{{ii}}}}}", param)
+
+    print(f"El mensaje a enviar es: {text_to_send}")
+
+    # Obtener la conversación abierta del contacto
+    open_conv = get_open_conversation(contacto_id)
+
+    # Verificar si se encontró una conversación abierta
+    if open_conv:
+        print(f"Se enlaza a la conversación: {open_conv}")
+        send_conversation_message(open_conv, text_to_send, buzon=buzon)
+    else:
+        print(f"No se encontró conversación para el contacto ID: {contacto_id}")
+
+        # Crear una nueva conversación si no hay una abierta
+        url = f"{base_url}/conversations"
+        headers = {
+            "Content-Type": "application/json",
+            "api_access_token": cw_token
+        }
+
+        if bot_name is None:
+            # Crear conversación sin bot
+            conversation_body = {
+                "contact_id": contacto_id,
+                "inbox_id": buzon,
+                "message": {
+                    "content": text_to_send
+                }
+            }
+        else:
+            # Crear conversación con bot
+            conversation_body = {
+                "contact_id": contacto_id,
+                "inbox_id": buzon,
+                "message": {
+                    "content": text_to_send
+                },
+                "custom_attributes": {
+                    "bot": bot_name
+                }
+            }
+
+        response = requests.post(url, json=conversation_body, headers=headers)
+        print(f"La respuesta al enviar la plantilla fue: {response.json()}")
+
+        if response.status_code == 200:
+            conversation_response = response.json()
+            if conversation_response and conversation_response.get("status") == "resolved":
+                reabrir_conversacion(conversation_response["id"])
+            print("Mensaje enviado con éxito.")
+        else:
+            print(f"Error al enviar mensaje: {response.text}")
+
+def reabrir_conversacion(conv_id):
+    """
+    Reabre una conversación en Chatwoot cambiando su estado a 'open'.
+    
+    :param conv_id: ID de la conversación a reabrir.
+    """
+    print(f"Reabriendo la conversación con ID: {conv_id}")
+    
+    url = f"{base_url}/conversations/{conv_id}/toggle_status"
+    headers = {
+        "Content-Type": "application/json",
+        "api_access_token": cw_token
+    }
+    
+    body = {
+        "status": "open"
+    }
+
+    response = requests.post(url, json=body, headers=headers)
+    
+    if response.status_code == 200:
+        print("Estatus cambiado con éxito.")
+    else:
+        print(f"Error al cambiar el estatus: {response.text}")
+
+def send_conversation_message(conversation_id, message, is_private=False, buzon=ChatwootSenders.Pacientes):
+    """
+    Envía un mensaje a una conversación en Chatwoot.
+    
+    :param conversation_id: ID de la conversación.
+    :param message: Mensaje a enviar.
+    :param is_private: Si es un mensaje privado o público.
+    :param buzon: ID del buzón de Chatwoot.
+    """
+    print(f"Enviando: {conversation_id} Msg: {message}")
+    
+    url = f"{base_url}/conversations/{conversation_id}/messages"
+    headers = {
+        "Content-Type": "application/json",
+        "api_access_token": cw_token
+    }
+    
+    message_body = {
+        "content": message,
+        "private": is_private
+    }
+
+    response = requests.post(url, json=message_body, headers=headers)
+    
+    if response.status_code == 200:
+        print(f"Mensaje enviado con éxito: {response.content.decode()}")
+    else:
+        print(f"Error al enviar mensaje: {response.text}")
+
 def get_open_conversation(contact_id):
     conv_id = None  # Inicia con None por si no se encuentra ninguna conversación
     url = f"{base_url}/conversations?status=open"
--- PyLibrary/test.py
+++ PyLibrary/test.py
@@ -1,7 +1,35 @@
-from CW_Conversations import get_open_conversation
+from CW_Conversations import ChatwootSenders, envia_mensaje_plantilla, get_open_conversation, send_conversation_message
 
-
+"""
+Probar get_open_conversation
 if __name__ == "__main__":
     contact_id = 181  # El ID del contacto que deseas probar
     conversation_id = get_open_conversation(contact_id)
     print(f"Conversation ID: {conversation_id}")
+"""
+
+"""
+Probar enviar mensaje
+
+if __name__ == "__main__":
+    conversation_id = 537  # Reemplaza con un ID de conversación válido
+    message = "Hola, @Pablo este es un mensaje de prueba, privado."  # Mensaje que deseas enviar
+    is_private = True  # Puedes cambiarlo a True si el mensaje es privado
+    buzon = ChatwootSenders.Pacientes  # O ChatwootSenders.Medicos, según sea necesario
+
+    # Llamar a la función para enviar el mensaje
+    send_conversation_message(conversation_id, message, is_private, buzon)
+"""
+
+
+contacto_id = 162  # Reemplaza con el ID del contacto
+plantilla = """Hola {{1}}, queremos ser siempre mejores para ti. Nos podrias decir como calificarías la atención brindada por tu medico {{2}}?
+
+por favor"""
+parametros = ["Juan", "de parte de la clínica."]
+buzon = ChatwootSenders.Pacientes  # O ChatwootSenders.Medicos
+bot_name = "Notificacion"
+
+envia_mensaje_plantilla(contacto_id, plantilla, parametros, buzon, bot_name)
+
+
