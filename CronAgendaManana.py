1f204be35a219b7aaa320d3d47c905feb1616c38
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2024-10-12 12:09:06 -0600

    f1
    se libera la automatizacion de la agenda de los medicos

---
+++ CronAgendaManana.py
@@ -0,0 +1,17 @@
+from libs.CW_Automations import SendBlast
+from libs.CW_Conversations import ChatwootSenders
+
+
+template_name = 'agenda_sumary2'
+buzon = ChatwootSenders.Medicos  # Instancia de la clase ChatwootSenders
+bot_name = 'AgendaMedico'  # Si no deseas usar un bot, puedes pasar None
+query = """SELECT        MedicoId,
+                    MedicoNickName,
+                    'Mañana' as Dia,
+                    MIN(FORMAT(start_datetime, 'HH:mm'))  AS Inicio,
+                    MAX(FORMAT(start_datetime, 'HH:mm'))  AS Fin
+        FROM            dbo.vwCalendario
+        WHERE        (CONVERT(date, start_datetime) = CONVERT(date, GETDATE() + 1))
+        GROUP BY MedicoID,MedicoNickName""" 
+
+SendBlast(template_name, buzon, bot_name, query)
