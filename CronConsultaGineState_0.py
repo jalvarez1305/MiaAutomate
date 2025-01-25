f0d7b053d950ddbd05883cbb0e67941f353139aa
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2025-01-25 13:39:07 -0600

    gyne state 0

---
+++ CronConsultaGineState_0.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'libs')))
+from CW_Automations import send_blast_image
+from CW_Conversations import ChatwootSenders
+from SQL_Helpers import ejecutar_update
+from CW_Contactos import actualizar_funel_states
+
+template_name = 'gine_state_0'
+buzon = ChatwootSenders.Pacientes  # Instancia de la clase ChatwootSenders
+bot_name = None  # Si no deseas usar un bot, puedes pasar None
+query = """SELECT        id, phone_number, name,GETDATE() as fecha,funel_state,'gine_state_0' as blast
+            FROM            CW_Contacts
+            WHERE        (custom_attributes_es_prospecto = 1) 
+                    AND (custom_attributes_interes_en = 'https://miaclinicasdelamujer.com/gynecologia/')
+                    AND funel_state =0
+                    AND datediff(hh,DATEADD(ss, [last_activity_at], '1970-01-01 00:00:00'),getdate())  > 50
+                    AND id not in (SELECT [Paciente ID]
+                                FROM [Clinica].[dbo].[vw_CalendarEventsExtracted]
+                                where DATEADD(hh, - 6, [start_datetime])>getdate() 
+                                and not [Paciente ID] is null)
+
+                    """ 
+
+#El query lleva, contacto, telefono y parametros
+send_blast_image(template_name, bot_name=bot_name, query=query)
+#Insertarlo en el historico de blast
+update_query ="""INSERT INTO [dbo].[FunnelHistory]
+                        ([id],[phone_number],[name],[fecha],[funel_state],[blast])
+                SELECT        id, phone_number, name,GETDATE() as fecha,funel_state,'gine_state_0' as blast
+                FROM            CW_Contacts
+                WHERE        (custom_attributes_es_prospecto = 1) 
+                        AND (custom_attributes_interes_en = 'https://miaclinicasdelamujer.com/gynecologia/')
+                        AND funel_state =0
+                        AND datediff(hh,DATEADD(ss, [last_activity_at], '1970-01-01 00:00:00'),getdate())  > 50
+                        AND id not in (SELECT [Paciente ID]
+                                    FROM [Clinica].[dbo].[vw_CalendarEventsExtracted]
+                                    where DATEADD(hh, - 6, [start_datetime])>getdate() 
+                                    and not [Paciente ID] is null)
+                        """
+ejecutar_update(update_query)
+# Iterar por cada contacto para acutlizar el funnel state a 1
+actualizar_funel_states(query,1)
--- libs/CW_Contactos.py
+++ libs/CW_Contactos.py
@@ -2,6 +2,8 @@ import requests
 import os
 from dotenv import load_dotenv
 
+from SQL_Helpers import execute_query
+
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
@@ -60,3 +62,47 @@ def actualizar_etiqueta(conv_id, label):
     else:
         print(f"Error al actualizar la etiqueta: {response.status_code}")
         print(conv_id)
+
+def actualizar_funel_state(contact_id, state):
+    # Configuración de la URL y encabezados de autenticación
+    url = f"{base_url}/contacts/{contact_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "api_access_token": cw_token
+    }
+    
+    # Datos para actualizar el atributo personalizado
+    data = {
+        "custom_attributes": {
+            "funel_state": state
+        }
+    }
+    
+    # Envío de la solicitud PATCH para actualizar el contacto
+    response = requests.put(url, json=data, headers=headers)
+    
+    # Verificación de la respuesta
+    if response.status_code == 200:
+        print("Atributo 'funnel_state' actualizado correctamente.")
+    else:
+        print(f"Error al actualizar el atributo: {response.status_code}")
+        print(response.json())
+
+def actualizar_funel_states(query=None,new_state=None):
+    """
+    Ejecuta la consulta y para cada fila busca el contacto y asi actualiza su state
+    """
+
+    # 2. Ejecutar el query para obtener el DataFrame
+    df = execute_query(query)
+    if df is None or df.empty:
+        print("No se obtuvieron resultados de la consulta o el resultado no es una tabla.")
+        return
+
+    # 3. Iterar sobre cada fila del DataFrame
+    for index, row in df.iterrows():
+        # 3.1 Extraer contacto_id y teléfono
+        contacto_id = row[0]  # Primera columna es contacto_id
+        # 5. Llamar a la función send_content_builder
+        actualizar_funel_state(contacto_id, new_state)
+    print("Contactos actualizados")
