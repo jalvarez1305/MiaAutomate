c32af563b54411556328eac830599f0f89ea65e0
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2025-01-25 11:28:17 -0600

    config

--- Bots/Bots_Config.py
+++ Bots/Bots_Config.py
@@ -1,2 +1,4 @@
-saludo_facebook="Hola. ¿Puedes darme más información sobre esto?"
+#saludo_facebook="Hola. ¿Puedes darme más información sobre esto?"
+facebook_2="¡Hola! Podrías darme más información"
+saludo_facebook="¡Hola! Podrías darme más información de..."
 audio_gyne="MandaAudio"
