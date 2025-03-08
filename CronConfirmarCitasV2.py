b61a759b951b516e74065adaf1ff94f19650c1c6
Diego Orlando Lopez Aguirre <dlopez@nanocode.mx>
2025-03-08 12:29:30 -0600

    confirmar

--- CronConfirmarCitasV2.py
+++ CronConfirmarCitasV2.py
@@ -1,27 +1,25 @@
-import os
-import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'libs')))
-from CW_Automations import SendBlast
-from CW_Conversations import ChatwootSenders
+from Blast.BlastHelper import SendBlast
 
-
-template_name = 'confirmar_v2'
-buzon = ChatwootSenders.Pacientes  # Instancia de la clase ChatwootSenders
-bot_name = "ConfirmarCitaBot" # Si no deseas usar un bot, puedes pasar None
+template_id = 'HXf14cab23bd6a7fac42a5e8cd7755d115'
+bot_name = "ConfirmarCitaBot"   # Si no deseas usar un bot, puedes pasar None
 query = """SELECT      [Paciente ID],
+			phone_number,
             Especialidad,
-            FORMAT(start_datetime, 'HH:mm') AS Hora,
             UPPER(LEFT(FORMAT(start_datetime, 'dddd', 'es-ES'), 1)) + LOWER(SUBSTRING(FORMAT(start_datetime, 'dddd', 'es-ES'), 2, LEN(FORMAT(start_datetime, 'dddd', 'es-ES')) - 1)) AS Dia,
             FORMAT(start_datetime, 'yyyy-MM-dd') AS Fecha,
+            FORMAT(start_datetime, 'HH:mm') AS Hora,
 			[MedicoNickName],
 			case 
-				when Especialidad = 'Cita Ginecologia' then 'Ropa ligera como vestido y zapatos facil de retirar como sandalia'
+				when Especialidad = 'Ginecologia' then 'Ropa ligera como vestido y zapatos facil de retirar como sandalia'
 				when Especialidad = 'Cita Nutricion' then ''
 				else ''
 			END as Recomendaciones
 FROM        dbo.vwCalendario
 WHERE       (CONVERT(date, start_datetime) = CONVERT(date, GETDATE() + 1))
+		and Not [Paciente ID] is null
+		and not phone_number is null
 and [Status Paciente]=0""" 
 
-SendBlast(template_name, buzon, bot_name, query,force_new=True)
+#El query lleva, contacto, telefono y parametros
+SendBlast(template_id, bot_name=bot_name, query=query)
