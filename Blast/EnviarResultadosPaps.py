89f9f271851ff48e635c1273810bd74c5afe0ec6
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2025-04-03 20:12:10 -0600

    Paps done

---
+++ Blast/EnviarResultadosPaps.py
@@ -0,0 +1,73 @@
+import os
+import sys
+from BlastHelper import SendBlast
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../libs')))
+from SQL_Helpers import execute_query,ExecuteScalar,ejecutar_update
+
+
+template_id = 'HX309b980d72c86b557050c96a5fec1735'
+bot_name = None  # Si no deseas usar un bot, puedes pasar None
+query = """
+        WITH CTE AS (
+        SELECT 
+            Docs.id AS MedicoID, 
+            Docs.phone_number, 
+            Docs.custom_attributes_nickname AS Medico, 
+            COALESCE(Docs.custom_attributes_nickname, Pacs.name) AS Paciente, 
+            CONVERT(VARCHAR(10), Paps.[Fecha Consulta], 23) AS Fecha, 
+            REPLACE(Paps.[presigned url], 'https://papanicolaous.s3.us-east-2.amazonaws.com/', '') AS Url,
+            ROW_NUMBER() OVER (PARTITION BY Docs.id ORDER BY Paps.[Fecha Consulta] ASC) AS RowNum
+        FROM dbo.Papanicolaous AS Paps
+        INNER JOIN dbo.CW_Contacts AS Docs ON Paps.Medico_FK = Docs.id
+        INNER JOIN dbo.CW_Contacts AS Pacs ON Paps.Paciente_FK = Pacs.id
+        WHERE 
+            Docs.id NOT IN (
+                SELECT DISTINCT Medico_FK 
+                FROM dbo.Papanicolaous WITH (NOLOCK) 
+                WHERE Estatus = N'Enviada al Medico'
+            )
+            AND Paps.Estatus = N'Enviada al Patologo'
+    )
+    SELECT MedicoID, 
+            phone_number, 
+            Medico, 
+            Paciente, 
+            Fecha, 
+            Url
+    FROM CTE
+    WHERE RowNum = 1
+        """ 
+def UpdateEstatus():
+    update_query="""
+                WITH CTE AS (
+                    SELECT 
+                        Paps.id AS PapanicolaousID, 
+                        Docs.id AS MedicoID, 
+                        ROW_NUMBER() OVER (PARTITION BY Docs.id ORDER BY Paps.[Fecha Consulta] ASC) AS RowNum
+                    FROM dbo.Papanicolaous AS Paps
+                    INNER JOIN dbo.CW_Contacts AS Docs ON Paps.Medico_FK = Docs.id
+                    WHERE 
+                        Docs.id NOT IN (
+                            SELECT DISTINCT Medico_FK 
+                            FROM dbo.Papanicolaous WITH (NOLOCK) 
+                            WHERE Estatus = N'Enviada al Medico'
+                        )
+                        AND Paps.Estatus = N'Enviada al Patologo'
+                )
+                UPDATE Paps
+                SET 
+                    Paps.[Fecha Medico] = GETDATE(), 
+                    Paps.Estatus = 'Enviada al Medico'
+                FROM dbo.Papanicolaous AS Paps
+                INNER JOIN CTE ON Paps.id = CTE.PapanicolaousID
+                WHERE CTE.RowNum = 1;
+                 """
+    ejecutar_update(update_query)
+#El query lleva, contacto, telefono y parametros
+SendBlast(template_id, bot_name=bot_name, query=query)
+UpdateEstatus()
+
+def SendPapToDoc():
+    SendBlast(template_id, bot_name=bot_name, query=query)
+    UpdateEstatus()
+
--- Bots/Bot_Paps.py
+++ Bots/Bot_Paps.py
@@ -6,6 +6,7 @@ import time
 # Añadir la ruta a libs al path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../libs')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../Blast')))
+from EnviarResultadosPaps import SendPapToDoc
 from BlastHelper import SendBlast
 from CW_Conversations import send_conversation_message, ChatwootSenders,send_audio_mp3_via_twilio, envia_mensaje_plantilla, remove_bot_attribute
 from CW_Contactos import actualizar_interes_en,actualizar_etiqueta
@@ -31,24 +32,21 @@ def BotPaps(Detalles):
         if conversation_id is None:
             logging.error("conversation_id no está presente en Detalles.")
             return
-        if last_message_content =='2.- PAP Negativo (receta)':
+        if last_message_content =='1.- Papanicolaou Negativo':
+            logging.info("1.- Papanicolaou Negativo")
+            NotificarPaciente('HX280bd888d556d826d10129f4d731625d',contact_id)#Pap negativo con receta
+        elif last_message_content =='2.- PAP Negativo (receta)':
             logging.info("2.- PAP Negativo (receta)")
-            NotificarPaciente('HXeb3ba5c4af5c76775adea115755ab53e',contact_id)#Pap negativo con receta
-            time.sleep(5)
             send_conversation_message(conversation_id, "Me puede compartir la receta, por favor", is_private=False, buzon=ChatwootSenders.Pacientes)
-        """
-        if last_message_content == saludo_facebook or last_message_content == audio_gyne:
-            MandarMensajeSaludo(conversation_id,contact_phone,contact_id)
-        elif last_message_content == 'Domicilio':
-            send_conversation_message(conversation_id, respuesta_ubicacion, is_private=False, buzon=ChatwootSenders.Pacientes)
-        else:
-            send_conversation_message(conversation_id, 'Hola soy Robot, me ayudan con esto?', is_private=True, buzon=ChatwootSenders.Pacientes)
-        """
+            NotificarPaciente('HXeb3ba5c4af5c76775adea115755ab53e',contact_id,True)#Pap negativo con receta
+        elif last_message_content =='3.- PAP Positivo VPH':
+            logging.info("3.- PAP Positivo VPH")
+            NotificarPaciente('HXbd5c3544d719abab4110d92e3c85e47c',contact_id)#Pap negativo con receta
     except Exception as e:
         logging.error(f"Error en AgendaBot: {str(e)}")  # Manejo de errores con logging
 
-def NotificarPaciente(template_sid, Medico_FK):
-    
+def NotificarPaciente(template_sid, Medico_FK,has_receta=False):
+    logging.info("Notificar al paciente")
     template_id = template_sid
     bot_name = None  # Si no deseas usar un bot, puedes pasar None
     query = f"""SELECT        Cont.id, Cont.phone_number, REPLACE(Paps.[presigned url], 'https://papanicolaous.s3.us-east-2.amazonaws.com/', '') AS url
@@ -59,6 +57,7 @@ def NotificarPaciente(template_sid, Medico_FK):
     #Se manda el mensaje apropiado a la paciente
     SendBlast(template_id, bot_name=None, query=query)
     #Se actualiza el estatus del papanicolaou
+    logging.info("Actualizar estatus")
     update_query =f"""UPDATE [dbo].[Papanicolaous]
                     SET [Estatus] = 'Enviada al paciente'
                         ,[Fecha Paciente] = getdate()
@@ -66,3 +65,7 @@ def NotificarPaciente(template_sid, Medico_FK):
                             AND Estatus = N'Enviada al Medico'
                    """
     ejecutar_update(update_query)
+    #Despues de actualizar el estatus del Pap, extraer un nuevo PAP para el doc y unicamente si no necesita receta, ya que hay que darle tiempo
+    if not has_receta:
+        logging.info("Mandar nuevo PAP al medico")
+        SendPapToDoc()
