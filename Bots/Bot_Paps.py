4b6d19e30223f35feffcc9e1b081c4ecd90e1e80
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2025-04-03 19:12:55 -0600

    local

--- Bots/Bot_Paps.py
+++ Bots/Bot_Paps.py
@@ -5,6 +5,8 @@ import time
 
 # Añadir la ruta a libs al path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../libs')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../Blast')))
+from BlastHelper import SendBlast
 from CW_Conversations import send_conversation_message, ChatwootSenders,send_audio_mp3_via_twilio, envia_mensaje_plantilla, remove_bot_attribute
 from CW_Contactos import actualizar_interes_en,actualizar_etiqueta
 from SQL_Helpers import execute_query,ExecuteScalar,ejecutar_update
@@ -31,6 +33,7 @@ def BotPaps(Detalles):
             return
         if last_message_content =='2.- PAP Negativo (receta)':
             logging.info("2.- PAP Negativo (receta)")
+            NotificarPaciente('HXeb3ba5c4af5c76775adea115755ab53e',contact_id)#Pap negativo con receta
             time.sleep(5)
             send_conversation_message(conversation_id, "Me puede compartir la receta, por favor", is_private=False, buzon=ChatwootSenders.Pacientes)
         """
@@ -44,38 +47,14 @@ def BotPaps(Detalles):
     except Exception as e:
         logging.error(f"Error en AgendaBot: {str(e)}")  # Manejo de errores con logging
 
-def MandarMensajeSaludo(conversation_id,contact_phone,contact_id):
+def NotificarPaciente(template_sid, Medico_FK):
     
-    # Actualiza el interes del contacto para que entre al funel
-    send_conversation_message(conversation_id,"Esperare 30 segundos antes de enviar el audio de bien venida",True)
-    actualizar_interes_en(contact_id, "https://miaclinicasdelamujer.com/gynecologia")
-    actualizar_etiqueta(conversation_id,"citagyne")
+    template_id = template_sid
+    bot_name = None  # Si no deseas usar un bot, puedes pasar None
+    query = f"""SELECT        Cont.id, Cont.phone_number, REPLACE(Paps.[presigned url], 'https://papanicolaous.s3.us-east-2.amazonaws.com/', '') AS url
+                FROM            dbo.Papanicolaous AS Paps WITH (NOLOCK) INNER JOIN
+                                        dbo.CW_Contacts AS Cont WITH (nolock) ON Paps.Paciente_FK = Cont.id
+                WHERE        (Paps.Medico_FK = {Medico_FK}) AND (Paps.Estatus = N'Enviada al Medico')""" 
 
-    current_time = datetime.now()
-
-    # Revisar si estamos entre las 10 PM y las 8 AM
-    if current_time.hour >= 22 or current_time.hour < 8:
-        send_conversation_message(conversation_id,"Hola Gracias por comunicarte a Mia clinicas de la mujer, nuestro horario de atencion es de 9:00 a 20:00, en breve nos comunicaremos contigo",False)
-        send_conversation_message(conversation_id,"A las 8am se enviara el audio pidiendo detalles",True)
-        print("a")
-
-        # Calcular el tiempo restante hasta las 8 AM
-        if current_time.hour >= 22:
-            # Si es después de las 10 PM, calcular el tiempo hasta las 8 AM del día siguiente
-            target_time = current_time.replace(hour=8, minute=0, second=0, microsecond=0) + timedelta(days=1)
-        else:
-            # Si es antes de las 8 AM, calcular el tiempo hasta las 8 AM del mismo día
-            target_time = current_time.replace(hour=8, minute=0, second=0, microsecond=0)
-
-        # Calcular el tiempo de espera
-        time_to_wait = (target_time - current_time).total_seconds()
-
-        # Hacer el delay
-        print(f"Esperando hasta las 8 AM... {time_to_wait} segundos restantes.")
-        time.sleep(time_to_wait)
-    else:
-        #Mandar el mensaje apropiado para simular que el humano acaba de despertar
-        time.sleep(30)
-    #despues de esperar lo necesario se manda el audio
-    send_audio_mp3_via_twilio(contact_phone,"https://ik.imagekit.io/etqfkh9q2/AudioBienvenidaMp3.mp3")   
-    send_conversation_message(conversation_id,"Se envio el audio preguntando por detalles",True)
+    #El query lleva, contacto, telefono y parametros
+    SendBlast(template_id, bot_name=None, query=query)
