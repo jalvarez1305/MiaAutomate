26da7d9a1203e694130c3fd4d8248f26cea50a94
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2025-05-04 12:47:59 -0600

    local

--- Bots/gyne_general.py
+++ Bots/gyne_general.py
@@ -116,31 +116,7 @@ def MandarMensajeSaludo(conversation_id,contact_phone,contact_id):
     actualizar_interes_en(contact_id, "https://miaclinicasdelamujer.com/gynecologia")
     actualizar_etiqueta(conversation_id,"citagyne")
 
-    current_time = datetime.now()
-
-    # Revisar si estamos entre las 10 PM y las 8 AM
-    if current_time.hour >= 22 or current_time.hour < 8:
-        send_conversation_message(conversation_id,"Hola Gracias por comunicarte a Mia clinicas de la mujer, nuestro horario de atencion es de 9:00 a 20:00, en breve nos comunicaremos contigo",False)
-        send_conversation_message(conversation_id,"A las 8am se enviara el audio pidiendo detalles",True)
-        print("a")
-
-        # Calcular el tiempo restante hasta las 8 AM
-        if current_time.hour >= 22:
-            # Si es después de las 10 PM, calcular el tiempo hasta las 8 AM del día siguiente
-            target_time = current_time.replace(hour=8, minute=0, second=0, microsecond=0) + timedelta(days=1)
-        else:
-            # Si es antes de las 8 AM, calcular el tiempo hasta las 8 AM del mismo día
-            target_time = current_time.replace(hour=8, minute=0, second=0, microsecond=0)
-
-        # Calcular el tiempo de espera
-        time_to_wait = (target_time - current_time).total_seconds()
-
-        # Hacer el delay
-        print(f"Esperando hasta las 8 AM... {time_to_wait} segundos restantes.")
-        time.sleep(time_to_wait)
-    else:
-        #Mandar el mensaje apropiado para simular que el humano acaba de despertar
-        time.sleep(30)
+    time.sleep(30)
     #despues de esperar lo necesario se manda el audio
     send_audio_mp3_via_twilio(contact_phone,"https://ik.imagekit.io/etqfkh9q2/AudioBienvenidaMp3.mp3")   
     send_conversation_message(conversation_id,"Cuéntame un poquito más ✨ ¿Tienes algún tema en especial que te gustaría revisar en la consulta 🩺💖 o ya te toca tu revisión ginecológica anual? 📅🌸",True)
