f5d3f076440b8d3afbaf18f149acc9b3148e32bc
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2025-05-29 12:52:25 -0600

    evaluacion recepcion

---
+++ CronEvaluaRecepcionMatutino.py
@@ -0,0 +1,10 @@
+from Blast.BlastHelper import SendBlast
+
+template_id = 'HX768f5ddd324413bc9adb3f076361c5ed'
+bot_name = None  # Si no deseas usar un bot, puedes pasar None
+query = """SELECT        id, phone_number
+            FROM            dbo.CW_Contacts
+            WHERE        (id = 1616)"""
+
+#El query lleva, contacto, telefono y parametros
+SendBlast(template_id, bot_name=bot_name, query=query)
