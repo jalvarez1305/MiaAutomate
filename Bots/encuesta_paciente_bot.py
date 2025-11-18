23a6e4d8a85bacfea101958950c53ddc9a0ce81a
PabloAlvarezVinco <135268286+PabloAlvarezVinco@users.noreply.github.com>
2025-11-17 19:19:25 -0600

    Update encuesta_paciente_bot.py

--- Bots/encuesta_paciente_bot.py
+++ Bots/encuesta_paciente_bot.py
@@ -44,7 +44,12 @@ def EncuestaPacienteBot(Detalles):
                 case _:
                     logging.warning("Bot no reconocido.")
             if calificacion == 5:
-                SendBlast("HXf83b08ae35dae1fef5f5283e0f4e1689", bot_name=None, query=query)
+                review_msg="""Que bie que tuviste una buena experiencia. Me seria muyt util si me ayudas a calificarnos en Google                
+Te dejo el link, ojal tengas la oportunidad de calificarnos: 
+
+https://g.page/r/CaJxTFA06462EBM/review"""
+                send_conversation_message(conversation_id, review_msg, is_private=False, buzon=ChatwootSenders.Pacientes)
+                #SendBlast("HXf83b08ae35dae1fef5f5283e0f4e1689", bot_name=None, query=query)
             else:
                 send_conversation_message(conversation_id, 'Que lamentable, me puedes comentar un poco de tu experiencia?, por favor', is_private=False, buzon=ChatwootSenders.Pacientes)
                 send_conversation_message(conversation_id, '@Pablo soy Robot, revisa esta calificacion', is_private=True, buzon=ChatwootSenders.Pacientes)
